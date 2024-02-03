dd82927b3bb2772617b61c2b7e0de53d9efd08c7
Amir Zahreddine <amir.zahreddine@devfacto.com>
2024-02-03 17:32:14 +0000

    WIP validation function for cubes

--- Day_02/Day_02.c
+++ Day_02/Day_02.c
@@ -12,6 +12,7 @@
 
 // function prototype
 void string_parser(const char *line);
+void validate_cube_toss(int cube_count, char *colour_letter);
 int get_game_number();
 void *safe_malloc(size_t n);
 FILE *getfile();
@@ -30,7 +31,7 @@ void main()
         // printf("Game %i results:\n", game_id); // WIP tracking
         // printf("%s\n", line);                  // WIP tracking
 
-        int game_valid = 0;    // flag to track validity of each game
+        int game_valid = 0; // flag to track validity of each game
         // char *games[100] = {}; // initialize array where games will be added to
 
         string_parser(line);
@@ -68,64 +69,71 @@ void main()
  * @param games: the array where each block represents a throw.
  * @return void, updates the games array via pointer to reflect groupings of each throw for further parsing
  */
-//Game 15532: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+// Game 15532: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 void string_parser(const char *line)
 {
     // variable declaration with definition
     static const char *game_delimeter = ";"; // delimeter indicating new hand of cubes within a game
     static const char *junk_delimeter = ":"; // delimeter indicating the split of where the content we want begins (to the right)
-    
+
     const int length_of_unparsed_line = strlen(line);
     const char *parsed_line_excluding_game_number = strstr(line, junk_delimeter) + 2; // passes line and junk_delimeter to strstr, returns all to the right of delim
-    
-    
+
     // printf("The games played are: %s\n", parsed_line_excluding_game_number);
 
+    // 36 blue, 4 red;
 
-// 36 blue, 4 red;
-
-    int x = 0; // index for the tmp_current_cube_count array
+    int x = 0;                                       // index for the tmp_current_cube_count array
     char *tmp_current_cube_count = safe_malloc(100); // resets t
 
     for (int i = 0; i < strlen(parsed_line_excluding_game_number); i++)
     {
         char current_character = parsed_line_excluding_game_number[i];
 
-        if (isdigit(current_character)){
+        if (isdigit(current_character))
+        {
 
             tmp_current_cube_count[x] = current_character;
-            
-            x++; 
-        } else if (current_character == 'b' || current_character == 'r' || current_character == 'g') { // if not digit, we assume we have the entire count for the specific colour. now validate.
+
+            x++;
+        }
+        else if (current_character == 'b' || current_character == 'r' || current_character == 'g')
+        { // if not digit, we assume we have the entire count for the specific colour. now validate.
 
             // printf("the original character is %c\n", current_character);
-            
+
             int converted_to_int_tmp_current_cube_count = atoi(tmp_current_cube_count);
-            printf("%i\n", converted_to_int_tmp_current_cube_count);
-            
-            switch (parsed_line_excluding_game_number[i]) {
-                case 'b': printf("Blue\n");
-                    i += 5;
-                    break;
-                case 'r': printf("Red\n");
-                    i += 4;
-                    break;
-                case 'g': printf("Green\n");
-                    i += 6;
-                    break;
+            // printf("%i\n", converted_to_int_tmp_current_cube_count);
+
+            char *colour_of_cube = safe_malloc(6);
+
+            switch (parsed_line_excluding_game_number[i])
+            {
+            case 'b':
+                colour_of_cube = "Blue";
+                // printf("Blue\n");
+                i += 5;
+                break;
+            case 'r':
+                colour_of_cube = "Red";
+                // printf("Red\n");
+                i += 4;
+                break;
+            case 'g':
+                colour_of_cube = "Green";
+                // printf("Green\n");
+                i += 6;
+                break;
             }
-            
-            
-            
-            
+
+            validate_cube_toss(converted_to_int_tmp_current_cube_count, colour_of_cube);
             memset(tmp_current_cube_count, 0, 100);
             x = 0;
         }
     }
-    
-    int game_number = get_game_number(line, parsed_line_excluding_game_number, length_of_unparsed_line);
-    printf("\n\nThe game number is: %i\n", game_number);
 
+    int game_number = get_game_number(line, parsed_line_excluding_game_number, length_of_unparsed_line);
+    printf("The game number is: %i\n", game_number);
 }
 
 /*
@@ -134,18 +142,17 @@ The first letter of the colour to validate against
 The current position within the for loop (i) so that this function will bump the for loop forward past the characters of the respective colour
 
 */
-int validate_cube(int cube_count, char colour_letter, int current_loop_position)
+void validate_cube_toss(int cube_count, char *cube_colour)
 {
-
-
+    printf("Cube Colour: %s\n", cube_colour);
+    printf("Cube number: %i\n\n", cube_count);
 }
-
 
 int get_game_number(const char *line, const char *parsed_line_excluding_game_number, const int length_of_unparsed_line)
 {
     const int length_of_parsed_line_excluding_game_number = strlen(parsed_line_excluding_game_number);
     const int idx_end_of_number = length_of_unparsed_line - length_of_parsed_line_excluding_game_number;
-    int idx_number_start = 5; // index begins one value to the left of the first digit
+    int idx_number_start = 5;                               // index begins one value to the left of the first digit
     const int idx_number_end_inclusive = idx_end_of_number; // index ends on the last digit
     const int game_number_length = idx_number_end_inclusive - idx_number_start + 1;
 
@@ -165,14 +172,11 @@ int get_game_number(const char *line, const char *parsed_line_excluding_game_num
 
     return int_number;
 
-    // printf("idx_end_of_number:\n%i\n\n", idx_end_of_number);  
-    // printf("idx_number_start:\n%i\n\n", idx_number_start);  
+    // printf("idx_end_of_number:\n%i\n\n", idx_end_of_number);
+    // printf("idx_number_start:\n%i\n\n", idx_number_start);
     // printf("idx_number_end_inclusive:\n%i\n\n\n", idx_number_end_inclusive);
-    // printf("game_number_length:\n%i\n\n\n", game_number_length);  
+    // printf("game_number_length:\n%i\n\n\n", game_number_length);
 }
-
-
-
 
 /**
  * @brief Wrapper around standard malloc function that handles memory allocation gracefully
