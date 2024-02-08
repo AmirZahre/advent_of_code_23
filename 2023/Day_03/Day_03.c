6c0d9280302605551d2b0c826bf1b47eeca495f1
Amir Zahreddine <amir.zahreddine@devfacto.com>
2024-02-08 18:16:11 +0000

    not capturing symbol directly after number

--- 2023/Day_03/Day_03.c
+++ 2023/Day_03/Day_03.c
@@ -21,7 +21,7 @@ struct number
 };
 
 // function prototypes
-void parse_string(char *line, int row_index, struct number *number_list);
+void parse_string(char *line, int row_index, struct number *number_list, int *n);
 
 int main(void)
 {
@@ -33,13 +33,16 @@ int main(void)
     int max_string_length = MAX_STRING_LENGTH;
     char *line = (char *)safe_malloc(sizeof(char) * max_string_length); // Use safe_malloc instead of malloc to allocate memory for each line
 
+    int n = 0; // intention is for n to be incrimented by 1 each time a number or symbol are encountered
     int row_index = 0;
     // Begin cycling through each line from within fp
     while (fgets(line, max_string_length, fp) != NULL)
     {
         // printf("row %i\n", row_index);
         // printf("\n%i\n", number_list[row_index].value);
-        parse_string(line, row_index++, number_list);
+        parse_string(line, row_index++, number_list, &n);
+        // printf("%d\n", n);
+        
     }
 
     // for (int i = 0; i < 15; i++){
@@ -49,14 +52,15 @@ int main(void)
     // printf("right %i left %i\n ", idx_left, idx_right);
 
     // printf("is_symbol: %i is_number: %i\n\n", number_list[row_index].is_symbol , number_list[row_index].is_number);
-
+    
+    
     fclose(fp);
     free(line);
 }
 
-void parse_string(char *line, int row_index, struct number *number_list)
+void parse_string(char *line, int row_index, struct number *number_list, int *n)
 {
-    int n = 0;
+
     int x = 0; // reset at each for loop - used to track consecutive int's
     char *tmp_number = safe_malloc(100);
 
@@ -65,11 +69,12 @@ void parse_string(char *line, int row_index, struct number *number_list)
 
     for (int i = 0; i < strlen(line); i++) // for each character within the provided line
     {
+        
         char current_character = line[i];
 
         if (current_character != '.' && current_character != '\n')
         {
-
+            // printf("%d\n", *n);  
             while (isdigit(current_character))
             {
                 tmp_number[x++] = current_character;
@@ -80,29 +85,29 @@ void parse_string(char *line, int row_index, struct number *number_list)
             int idx_right = i;
             int int_current_num = atoi(tmp_number);
 
-            // printf("%i\n", n);
-            number_list[n].value = int_current_num;
+            // printf("%i\n", row_index);
+            number_list[row_index].value = int_current_num;
+            // printf("%i\n\n", number_list[row_index].value);
 
             if (int_current_num == 0)
             {
-                number_list[n].is_symbol = 1;
-                number_list[n].is_number = 0;
+                number_list[row_index].is_symbol = 1;
+                number_list[row_index].is_number = 0;
             }
             else
             {
-                number_list[n].is_number = 1;
-                number_list[n].is_symbol = 0;
+                number_list[row_index].is_number = 1;
+                number_list[row_index].is_symbol = 0;
             }
 
-        
             printf("%i\n ", int_current_num);
             printf("right %i left %i\n ", idx_left, idx_right);
 
-            printf("is_symbol: %i is_number: %i\n\n", number_list[n].is_symbol , number_list[n].is_number);
-            n++;
-        }
+            printf("is_symbol: %i is_number: %i\n\n", number_list[*n].is_symbol , number_list[*n].is_number);
 
-        
+            (*n)++;
+            
+        }
 
         memset(tmp_number, 0, 100);
         x = 0;
