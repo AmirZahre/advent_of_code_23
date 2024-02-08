6e869affc20f350cea99884438daa9c3ff9f48b2
Amir Zahreddine <amir.zahreddine@devfacto.com>
2024-02-08 15:25:54 +0000

    before array move to pointer

--- 2023/Day_03/Day_03.c
+++ 2023/Day_03/Day_03.c
@@ -10,19 +10,22 @@
 #define MAX_STRING_LENGTH 256
 #define MAX_NUMBERS 1000
 
-// function prototypes
-void capture_number(char *line, int row_index);
-
 struct number
 {
-    int number;
+    int value;
+    int is_number;
+    int is_symbol;
     int idx_left;
     int idx_right;
     int valid_number;
-} number_list[MAX_NUMBERS];
+};
+
+// function prototypes
+void parse_string(char *line, int row_index, struct number *number_list);
 
 int main(void)
 {
+    struct number number_list[MAX_NUMBERS];
 
     char *filename = "/workspaces/advent_code_23/2023/Day_03/" FILE_NAME; // Specify .txt file
     FILE *fp = getFile(filename);
@@ -35,68 +38,76 @@ int main(void)
     while (fgets(line, max_string_length, fp) != NULL)
     {
         // printf("row %i\n", row_index);
-        capture_number(line, row_index++);
-
+        parse_string(line, row_index++, number_list);
     }
 
-    // int lineNumber = 0;
+    // for (int i = 0; i < 15; ){
+    //     printf("\n%i\n", number_list[i].value);
+    //     i++;
+    // };
+            // printf("%i\n ", int_current_num);
+            // printf("right %i left %i\n ", idx_left, idx_right);
 
-    // // Iterate through each line in the file using fgets
-    // for (; fgets(line, sizeof("fsdfatrtsfdsf"), fp) != NULL ;) {
-    //     // Process the line (e.g., print it with the line number)
-    //     printf("Line %d: %s\n", lineNumber++, line);
-    // }
+            // printf("is_symbol: %i is_number: %i\n\n", number_list[row_index].is_symbol , number_list[row_index].is_number);
+
 
     fclose(fp);
     free(line);
 }
 
-void capture_number(char *line, int row_index)
+void parse_string(char *line, int row_index, struct number *number_list)
 {
-    int x = 0;
+    int n = 0;
+    int x = 0; // reset at each for loop - used to track consecutive int's
     char *tmp_number = safe_malloc(100);
 
+    // struct number number_list[MAX_NUMBERS];
+    // number_list[2].idx_left = 5;
 
     for (int i = 0; i < strlen(line); i++)
     {
         char current_character = line[i];
 
-    if (current_character != '.' && current_character != '\n') {
+        if (current_character != '.' && current_character != '\n')
+        {
 
-        while (isdigit(current_character)){
-            tmp_number[x++] = current_character;
-            current_character = line[++i];
+            while (isdigit(current_character))
+            {
+                tmp_number[x++] = current_character;
+                current_character = line[++i];
+            }
+
+            int idx_left = i - strlen(tmp_number);
+            int idx_right = i;
+            int int_current_num = atoi(tmp_number);
+
+            number_list[n].value = int_current_num;
+
+            if (int_current_num == 0) {
+                number_list[n].is_symbol = 1;
+                number_list[n].is_number = 0;
+            } else {
+                number_list[n].is_number = 1;
+                number_list[n].is_symbol = 0;
+            }
+
+            printf("%i\n ", int_current_num);
+            printf("right %i left %i\n ", idx_left, idx_right);
+
+            printf("is_symbol: %i is_number: %i\n\n", number_list[n].is_symbol , number_list[n].is_number);
+
         }
 
-        
-        int idx_left = i - strlen(tmp_number);
-        int idx_right = i;
-        int int_current_num = atoi(tmp_number);
-        printf("%i: ", int_current_num);
-        printf("right %i left %i\n", idx_left, idx_right);
+        n++;
+
+        memset(tmp_number, 0, 100);
+        x = 0;
+
+    }
+    // for (int i = 0; i < 12; i++){
+    //     printf("is number: %i is symbol: %i\n", number_list[i].is_number, number_list[i].is_symbol);
+    //     }
+
     }
 
-        // if (isdigit(current_character))
-        // {
 
-        //     tmp_number[x++] = current_character;
-
-        //     // printf("left %c\n",  line[i-1]);
-
-
-             
-        //     // printf("left %c\n",  line[i+1]);
-        // }
-        // else 
-        // {           
-        //     int int_current_num = atoi(tmp_number);
-        //     printf("%i\n", int_current_num);
-
-
-
-            memset(tmp_number, 0, 100);
-            x = 0;
-        // // }
-        // }
-    }
-}
